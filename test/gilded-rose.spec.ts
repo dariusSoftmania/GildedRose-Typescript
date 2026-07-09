5a755bc8fc74895f143452f5a00cf377a0155431
Darius Floroiu <Darius.Floroiu@softwire.com>
2026-07-09 14:34:16 +0300

    add tests for "Aged Brie"

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -6,7 +6,31 @@ describe('Gilded Rose', function () {
     it('should foo', function() {
         const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+        expect(items[0].name).to.equal('foo');
+    });
+
+    it("Aged Brie quality increase", function () {
+        const gildedRose = new GildedRose([ new Item("Aged Brie", 5, 40) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(41);
+    });
+
+    it("Aged Brie quality increase more if on due date", function () {
+        const gildedRose = new GildedRose([ new Item("Aged Brie", 0, 40) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(42);
+    });
+
+    it("Aged Brie quality increase more if expired", function () {
+        const gildedRose = new GildedRose([ new Item("Aged Brie", -2, 40) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(42);
+    });
+
+    it("Aged Brie quality under 50", function () {
+        const gildedRose = new GildedRose([ new Item("Aged Brie", -2, 49) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(50);
     });
 
 });
